df69a078b322f42bdf286c5af6c3337b44ee1e27
Nilesh Nartam <nileshnartam@gmail.com>
2020-02-06 21:37:42 +0530

    add code into repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'gallary', loadChildren: () => import('./gallary/gallary.module').then(m => m.GallaryModule) }, 
+  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }, 
+  { path: 'locations', loadChildren: () => import('./locations/locations.module').then(m => m.LocationsModule) }, 
+  { path: 'contacts', loadChildren: () => import('./contacts/contacts.module').then(m => m.ContactsModule) },
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
